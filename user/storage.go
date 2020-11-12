70690165d103fb27e7770fad6954cddc8d5dd65e
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-11 20:32:38 -0500

    methods for user table

---
+++ user/storage.go
@@ -0,0 +1,162 @@
+package user
+
+import (
+	"database/sql"
+	"github.com/Leonardo-Antonio/api-profiles/helper"
+	"github.com/asaskevich/govalidator"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func NewStorage(db *sql.DB) *Storage {
+	return &Storage{db}
+}
+
+func (s *Storage) GetAll() (users []Model, err error) {
+	stmt, err := s.db.Prepare(sqlGetAll)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	profileNull := sql.NullString{}
+
+	for rows.Next() {
+		user := Model{}
+		err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.LastName,
+			&user.Phone,
+			&user.Email,
+			&user.Password,
+			&profileNull,
+		)
+		if err != nil {
+			return users, err
+		}
+		user.Profile = profileNull.String
+		users = append(users, user)
+	}
+	return
+}
+
+func (s *Storage) SingUp(data Model) error {
+	stmt, err := s.db.Prepare(sqlSignUp)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	if !govalidator.IsEmail(data.Email) {
+		return helper.ErrEmailInvalid
+	}
+	if len(data.Name) < 1 && len(data.LastName) < 1 {
+		return helper.ErrPersonalInformationInvalid
+	}
+	if len(data.Password) < 10 {
+		return helper.ErrInsecurePassword
+	}
+	rs, err := stmt.Exec(
+		helper.NullString(data.Name),
+		helper.NullString(data.LastName),
+		helper.NullString(data.Phone),
+		helper.NullString(data.Email),
+		helper.NullString(data.Password),
+		helper.NullString(data.Profile),
+	)
+	if err != nil {
+		return err
+	}
+
+	rA, err := rs.RowsAffected()
+	if rA != 1 {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Storage) SingIn(identification Model) (data Model, err error) {
+	stmt, err := s.db.Prepare(sqlSignIn)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	profileNull := sql.NullString{}
+
+	err = stmt.QueryRow(identification.Email, identification.Password).Scan(
+		&data.ID,
+		&data.Name,
+		&data.LastName,
+		&data.Phone,
+		&data.Email,
+		&profileNull,
+	)
+	data.Profile = profileNull.String
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *Storage) Delete(identification Model) error {
+	stmt, err := s.db.Prepare(sqlDelete)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	rs, err := stmt.Exec(identification.Email, identification.Password)
+	if err != nil {
+		return err
+	}
+	rA, err := rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rA != 1 {
+		return helper.ErrUserInvalid
+	}
+	return nil
+}
+
+func (s *Storage) Update(data Model) error {
+	stmt, err := s.db.Prepare(sqlUpdate)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	if len(data.Name) < 1 && len(data.LastName) < 1 {
+		return helper.ErrPersonalInformationInvalid
+	}
+	rs, err := stmt.Exec(
+		helper.NullString(data.Name),
+		helper.NullString(data.LastName),
+		helper.NullString(data.Phone),
+		helper.NullString(data.Profile),
+		data.ID,
+	)
+	if err != nil {
+		return err
+	}
+
+	rA, err := rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rA != 1 {
+		return helper.ErrRowNotAffected
+	}
+	return nil
+}
