549551b55d5c5a1d8014ab74d6c6b8078af354c0
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-11 20:25:21 -0500

    structure of the table tb_users

---
+++ user/model.go
@@ -0,0 +1,11 @@
+package user
+
+type Model struct {
+	ID       int    `json:"id" xml:"id"`
+	Name     string `json:"name" xml:"name"`
+	LastName string `json:"last_name" xml:"last_name"`
+	Phone    string `json:"phone" xml:"phone"`
+	Email    string `json:"email" xml:"email"`
+	Password string `json:"password" xml:"password"`
+	Profile  string `json:"profile" xml:"profile"`
+}
