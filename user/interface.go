75e05e78c7e519e5598a43fd1066f24df866f54a
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-12 11:19:59 -0500

    change names in methods

--- user/interface.go
+++ user/interface.go
@@ -2,8 +2,8 @@ package user
 
 type iuser interface {
 	GetAll() (users []model, err error)
-	SingUp(data model) error
-	SingIn(identification model) (data model, err error)
+	SignUp(data model) error
+	SignIn(identification model) (data model, err error)
 	Delete(identification model) error
 	Update(data model) error
 }
